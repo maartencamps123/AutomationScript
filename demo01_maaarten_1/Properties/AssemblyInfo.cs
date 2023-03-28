2ee732604c075ab879f434e50adb828101c5e7de
mcamps <mcamps@PPC18578>
2023-03-28 14:06:38 +0200

    Add project files.

---
+++ demo01_maaarten_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("demo01_maaarten_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("demo01_maaarten_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("93EB2C08-E061-4620-8598-4093199F220B")]
+[assembly: AssemblyVersion("1.0.0.0")]
